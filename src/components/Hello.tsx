862f2f9315b243dba60da248e257f47663fe76db
Jalyna <jalyna.schroeder@gmail.com>
2019-01-22 17:35:13 +0100

    setup styled components

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,25 @@
-import * as React from "react";
+import * as React from 'react'
+import styled from 'styled-components'
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface HelloProps { 
+  compiler: string
+  framework: string
+}
+
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
-export class Hello extends React.Component<HelloProps, {}> {
+export default class Hello extends React.Component<HelloProps, {}> {
   render() {
-    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    return <h1><Button>Helloe</Button> from {this.props.compiler} and {this.props.framework}!</h1>
   }
 }
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
