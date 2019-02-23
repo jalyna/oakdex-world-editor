eec6f7598d772d15796f315d792399f5a837180b
Jalyna <jalyna.schroeder@gmail.com>
2019-02-23 10:23:54 +0100

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,15 @@ module.exports = {
     "json",
     "node"
   ],
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__tests__/fileMock.js",
+    "^shared/(.*)": "<rootDir>/src/shared/$1",
+    "^components/(.*)": "<rootDir>/src/components/$1",
+  },
+  "moduleDirectories": [
+    "node_modules",
+    "src"
+  ],
   "snapshotSerializers": ["enzyme-to-json/serializer"],
   "setupTestFrameworkScriptFile": "<rootDir>/src/setupEnzyme.ts"
 }
